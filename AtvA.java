b845dfc05b65d12bc9e247bd6aa784f302bb55c9
Blacklinkker <lucasmonir@hotmail.com>
2020-04-29 11:54:22 -0300

    trabalho popov lista simples (ate ai td bem)

--- AtvA.java
+++ AtvA.java
@@ -1,6 +1,29 @@
 package Trab2;
-// import javax.swing.JOptionPane;
+//biblioteca que fornece janelas de mensgaem pré-montadas
+import javax.swing.JOptionPane;
 
 public class AtvA{
-    
+    public static void main(String[] args) {
+        //lista do tipo segmento criada
+        Segmento lista=new Segmento();
+        int tamanho=0;
+        //lista pre-preenchida para facilitar testes de inicio meio e fim
+        lista.addInicio("John eckert");
+        tamanho++;
+        lista.addMeio(1,"Von neumann");
+        tamanho++;
+        lista.addFim("Alan turing");
+        tamanho++;
+        lista.addFim("linus torvalds");
+        tamanho++;
+        
+        JOptionPane.showMessageDialog(null,"Lista:"+lista.toString());
+        JOptionPane.showMessageDialog(null,"removendo meio: "+lista.removeMeio(1));
+        JOptionPane.showMessageDialog(null,"removendo inicio: "+lista.removeInicio());
+        JOptionPane.showMessageDialog(null,"removendo fim:"+lista.removeFim());
+        JOptionPane.showMessageDialog(null,"removendo meio:"+lista.removeMeio(1));
+        
+
+        
+    }
 }
--- Segmento.java
+++ Segmento.java
@@ -17,10 +17,47 @@ public class Segmento {
         if(primeiro==null){
             return "Lista vazia";
         }
-        String nomeAux=primeiro.nome;
+        String aux=primeiro.nome;
         tamanho--;
-        return nomeAux;   
+        return aux;   
     }
+    public void addMeio(int posicao,String nome){
+        if(posicao<=0){
+            addInicio(nome);
+        }else if(posicao>=tamanho){
+            addFim(nome);
+        }else{
+            Segmento aux=primeiro;
+            for(int i=0;i<posicao-1;i++){
+                aux=aux.proximo;
+            }
+            Segmento segmento=new Segmento();
+            segmento.nome=nome;
+            segmento.proximo=segmento;
+            tamanho++;
+        }
+    }
+
+    public String removeMeio(int posicao){
+        if(tamanho==0 || posicao>tamanho){
+            return null;
+        }else if(posicao==0){
+            return removeInicio();
+        }else if(posicao==tamanho-1){
+            return removeFim();
+        }else{
+            Segmento aux=primeiro;
+            for(int i=0;i<posicao-1;i++){
+                aux=aux.proximo;
+            }
+            String nome=aux.proximo.nome;
+            aux.proximo=aux.proximo.proximo;
+            tamanho--;
+            return nome;
+        }
+    }
+
+    
     public void addFim(String nome){
         Segmento segmento = new Segmento();
         segmento.nome=nome;
@@ -34,6 +71,33 @@ public class Segmento {
             aux.proximo=segmento;
             segmento.proximo=null;
             tamanho++;
+        
         }
+    }
+    public String removeFim(){
+        if(primeiro==null){
+            return null;
+        }
+        Segmento aux=primeiro;
+        while(aux.proximo!=null){
+            aux=aux.proximo;
+            if(aux.proximo==null){
+                tamanho--;
+                return aux.nome;
+            }
+        }
+        primeiro=null;
+        tamanho--;
+        return aux.nome;
+    }
+    @Override
+    public String toString(){
+        String compilado="";
+        Segmento aux=primeiro;
+        while(aux!=null){
+            compilado+=aux.nome+",";
+            aux=aux.proximo;
+        }
+        return compilado;
     }
 }
