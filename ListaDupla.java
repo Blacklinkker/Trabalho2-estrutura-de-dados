3f6f51d7152b3057f6a9c0e0f109f2c619346505
Lucas <55569386+Blacklinkker@users.noreply.github.com>
2020-05-01 12:18:19 -0300

    trabalho 2-Atividade 1 parte 2-lista dupla (usa segmento2)
    (usa segmento2)

--- ListaDupla.java
+++ ListaDupla.java
@@ -1,4 +1,22 @@
 package LD;
+//importa biblioteca para exibição de caixas de dialogo
+import javax.swing.*;
 public class ListaDupla {
-    
+    public static void main(String[] args) {
+        //Instanciação da lista duplamente encadeada
+        Segmento2 listadupla=new Segmento2();
+
+        //chamada dos metodos de adição
+        listadupla.addInicio("Von neumann"); //adiciona no inicio
+        listadupla.addFim("Alan turing"); //adiciona no fim
+        listadupla.addMeio(1,"Linus torvalds"); //adiciona no meio
+        
+        //exibição da lista
+        JOptionPane.showMessageDialog(null, "Lista:"+listadupla.toString());
+
+        //chamada dos metodos de remoção
+        JOptionPane.showMessageDialog(null,listadupla.removeMeio(1)); //remove no meio
+        JOptionPane.showMessageDialog(null,listadupla.removeInicio()); //remove no inicio
+        JOptionPane.showMessageDialog(null,listadupla.removeFim()); //remove no fim
+    }
 }
